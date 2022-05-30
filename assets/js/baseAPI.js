6e56859841286740f88245c1e6117929a1388313
vikkity <2878964251@qq.com>
2022-05-30 20:33:57 +0800

    完成主页面

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,19 @@
 $.ajaxPrefilter((option) => {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     option.url = `http://www.liulongbin.top:3007` + option.url;
+    // 统一为有权限的接口，设置 headers 请求头
+    if(option.url.indexOf("/my/")){
+      option.headers = {
+        Authorization: localStorage.getItem("token"),
+    };
+    }
+
+    option.complete=(res)=>{
+      if(res.responseJSON.status ===1 && res.responseJSON.message === "身份认证失败！"){
+        //强制清空
+        localStorage.removeItem("token");
+        //强制跳转到登录页面
+        location.href = "/login.html"
+      }
+    }
   });
---
+++ assets/js/index.js
@@ -0,0 +1,44 @@
+function getUserInfo(){
+    $.ajax({
+        type: "GET",
+        url:"/my/userinfo",
+        // headers: {
+            // Authorization: localStorage.getItem("token"),
+        // },
+        success: (res) => {
+            console.log(res);
+            if(res.status  !==0)return layer.msg("获取信息失败")
+            layer.msg("获取信息成功")
+            renderAvatar(res.data)
+        }
+    })
+}
+
+const renderAvatar =(user)=>{
+     // 获取用户名字
+     let name = user.nickname || user.username
+      // 设置欢迎文本
+      $('#welcome').html(`欢迎${name}`)
+       // 按需渲染用户头像
+       if(user.user_pic != null){
+             // 渲染图片头像
+        $(".layui-nav-img").attr("src", user.user_pic).show();
+        $(".text-avatar").hide();
+       }else {
+           $('.layui-nav-img').hide()
+           let firstName = name[0].toUpperCase();
+           $(".text-avatar").html(firstName);
+       }
+}
+
+//退出功能
+$('#btnlogout').click(()=> {
+    layer.confirm('是否退出？',{ icon: 3, title: "" }, function (index) {
+        // 清空本地存储里面的 token
+        localStorage.removeItem("token");
+        // 重新跳转到登录页面
+        location.href = "/login.html";
+    })
+})
+
+getUserInfo()
